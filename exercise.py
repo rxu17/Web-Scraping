97faad986efc60b2f1ea4110ffb8f4d2373952cf
rxu17 <rxu17@uw.edu>
2018-02-14 02:38:08 -0800

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -1,7 +1,7 @@
 """
-Your task is to write a python program to do the following:
+The goal of the python program was to do the following:
     1) For each inspection for each facility on a single page of results from the Napa county health
-       department website (url given below), scrape the following information:
+       department website (url given below), we scraped the following information:
        - Facility name
        - Address (just street info, not city, state, or zip)
        - City
@@ -12,11 +12,8 @@ Your task is to write a python program to do the following:
        - For each out-of-compliance violation type, scrape the violation type number and corresponding description.
          For example, an inspection might contain violation type numbers 6 and 7, and descriptions
          "Adequate handwashing facilities supplied & accessible" and "Proper hot and cold holding temperatures"
-    2) Place this information in a database of some sort. You can use whatever you want; sqlite, postgres, mongodb, etc.
-       Organize the data in a fashion which seems the most logical and useful to you. Include in your result the
-       necessary instructions to set up this database, such as create table statements.
-    3) Fetch this information from the database, and print it to the console in some fashion which clearly
-       and easily displays the data you scraped.
+    2) We then placed this information in a sqlite3 database.
+    3) We then query this info from the database, and print it to the console using pandas
 """
 
 import sqlite3
